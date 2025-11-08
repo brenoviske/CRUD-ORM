dae9dd9e50f0036ae96ea97c19f6b592607668fd
Breno Reis <reisb6774@gmail.com>
2025-11-07 21:58:24 -0300

    fix: Removing the class methods to create them in the ManagerController class

--- management/models.py
+++ management/models.py
@@ -1,11 +1,12 @@
 from sqlalchemy import Column , String , create_engine, ForeignKey
-from sqlalchemy.orm import declarative_base , sessionmaker , relationship 
+from sqlalchemy.orm import relationship
+from base import Base , engine
 
 
-# Declaring the DataBase here
-db = declarative_base()
+Base.metadata.create_all(engine)
 
-class Manager(db):
+# ------- Creating the Manager table in the database ----- #
+class Manager(Base):
 
     __tablename__ = 'managers'
 
@@ -15,45 +16,15 @@ class Manager(db):
 
     users = relationship("User",back_populates="manager")
 
-
-    def __repr__(self):
-        return f'<(Email ={self.email},password={self.password},sector={self.sector})'
-    
-
-    def getUsers(self)-> list[str]:
-        if not self.users : 
-            return []
-
-        return [user.getName() for user in self.users]
-    
-    def getCount(self) -> int : return len(self.users) if self.users else 0
+    def to_dict(self):
+        return {
+            "email" : self.email,
+            "sector" : self.sector,
+        }
 
 
-    def addUser(self,user:"User") -> None:
-        
-        existing_user = session.query(User).filter_by(cpf = user.getCPF()).first()
-
-        if existing_user:
-            print("This user already exists inside our database")
-        else:
-            session.add(user)
-            session.commit()
-            print("User sucessfully added")
-
-    def removerUser(self,cpf:str) -> None:
-        
-        existing_user = session.query(User).filter_by(cpf = cpf).first()
-
-        if not existing_user:
-            print(f"The user does not exists in our Database")
-        else:
-            session.delete(existing_user)
-            session.commit()
-            
-            print("The user was successfully deleted")
-
-class User(db):
-    
+# ---------- Creating the User table in the database ------ #
+class User(Base):
     __tablename__ = 'users'
 
     cpf = Column( String(11), primary_key = True , nullable = False)
@@ -63,24 +34,11 @@ class User(db):
     manager_email = Column(String(200),ForeignKey("managers.email"))
     manager = relationship("Manager",back_populates = "users")
 
-    def __repr__(self):
-        return f'<(cpf = {self.cpf}, Name = {self.name} , Sector = {self.sector})>'
-    
+    def to_dict(self):
+        return {
+            "cpf" : self.cpf,
+            "name" : self.name,
+            "sector" : self.sector,
+            "manager_email" : self.manager_email,
+        }
 
-    def getCPF( self ) -> str:
-        return self.cpf
-    
-
-    def getName( self ) -> str:
-        return self.name
-    
-    def getSector( self ) -> str:
-        return self.sector
-        
-
-engine = create_engine("mysql+pymysql://root:Ivimorcega1@localhost:3306/management")
-
-Session = sessionmaker( bind = engine )
-session = Session()
-
-db.metadata.create_all(engine)
